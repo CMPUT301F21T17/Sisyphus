0bbf968b65ebf689ace68e0f4e6e97ec9de76cc4
Junrui Zhu <junrui2@ualberta.ca>
2021-10-28 16:50:37 -0600

    combine sean's database

--- app/src/main/java/com/example/sisyphus/MainActivity.java
+++ app/src/main/java/com/example/sisyphus/MainActivity.java
@@ -46,17 +46,17 @@ public class MainActivity extends AppCompatActivity {
     public void addHabitEvent(String TAG) {
         FirebaseFirestore db = FirebaseFirestore.getInstance();
         final CollectionReference collectionReference = db.collection("Users");
-        String userID = "jqrBkABHBSWzCKv4DoP5";
-        String habitID = "s7iYT2nQSDg52bngNE8y";
+        String userID = "JuruiTest";
+        String habitID = "Swimming";
         Date testDate = new Date();
         HabitEvent newEvent = new HabitEvent("Testing", testDate);
-        User testUser = new User("Not Sean", "Not Paetz", testDate);
+        //User testUser = new User("Not Sean", "Not Paetz", testDate);
         //Habit testHabit = new Habit("Testing Firebase", "Please work!", 1234567, testDate);
         collectionReference
                 //.document(userID).collection("Habits").document(habitID).collection("HabitEvent").document("temp").set(newEvent)
                 //.document("Temp User").set(testUser)
                 //.document(userID).collection("Habits").document("TestHabit").set(testHabit)
-                .document(userID).collection("Habits").document("TestHabit").collection("HabitEvent").document("NestedTest").set(newEvent)
+                .document(userID).collection("Habits").document(habitID).collection("HabitEvent").document("NestedTest").set(newEvent)
                 .addOnSuccessListener(new OnSuccessListener<Void>() {
                     @Override
                     public void onSuccess(Void aVoid) {
