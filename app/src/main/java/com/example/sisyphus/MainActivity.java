b8c8d6fddf689dfd43ab41e7b987fe217e6eea6f
KaelenB <ktbrown@ualberta.ca>
2021-11-01 17:20:57 -0600

    testing

--- app/src/main/java/com/example/sisyphus/MainActivity.java
+++ app/src/main/java/com/example/sisyphus/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.sisyphus;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        FirebaseAuth mauth = new FirebaseAuth();
+        Intent i = new Intent(MainActivity.this, CalendarActivity.class);
+        i.putExtra("MAUTH", mauth);
+
     }
 }
