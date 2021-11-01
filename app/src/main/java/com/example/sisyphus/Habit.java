58bd08454df06aab790c3d5171ec0566c92ae059
KaelenB <ktbrown@ualberta.ca>
2021-11-01 16:18:31 -0600

    added AllHabitList_Adapter     modified CalendarActivity to work with it     modified Habit

--- app/src/main/java/com/example/sisyphus/Habit.java
+++ app/src/main/java/com/example/sisyphus/Habit.java
@@ -1,15 +1,27 @@
 package com.example.sisyphus;
 
+import android.text.Editable;
+
+import java.io.Serializable;
+import java.time.DayOfWeek;
 import java.util.Calendar;
+import java.util.Date;
 import java.util.EnumSet;
+import java.util.Locale;
 
-public class Habit {
-    private EnumSet<DaysRepeated> daysRepeated;
-    private Calendar dateStarted;
+public class Habit implements Serializable {
     private String name;
+    private String reason;
+    private Calendar date;
+    private EnumSet<DayOfWeek> daysRepeated;
 
-    public Habit(Calendar dateStarted, EnumSet<DaysRepeated> daysRepeated) {
-        this.dateStarted = dateStarted;
+    public Habit() {
+    }
+
+    public Habit(String name, Calendar date, String reason, EnumSet<DayOfWeek> daysRepeated) {
+        this.name = name;
+        this.reason = reason;
+        this.date = date;
         this.daysRepeated = daysRepeated;
     }
 
@@ -17,11 +29,36 @@ public class Habit {
         return name;
     }
 
-    public EnumSet<DaysRepeated> getDaysRepeated() {
+    public String getReason() {
+        return reason;
+    }
+
+    public String getDate() {
+        String result = date.getDisplayName(Calendar.YEAR, Calendar.ALL_STYLES, Locale.CANADA);
+        result += "-";
+        result += date.getDisplayName(Calendar.MONTH, Calendar.ALL_STYLES, Locale.CANADA);
+        result += "-";
+        result += date.getDisplayName(Calendar.DATE, Calendar.ALL_STYLES, Locale.CANADA);
+        return result;
+    }
+
+    public EnumSet<DayOfWeek> getDaysRepeated() {
         return daysRepeated;
     }
 
-    public Calendar getDateStarted() {
-        return this.dateStarted;
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setReason(String reason) {
+        this.reason = reason;
+    }
+
+    public void setDate(Calendar date) {
+        this.date = date;
+    }
+
+    public void setDaysRepeated(EnumSet<DayOfWeek> daysRepeated) {
+        this.daysRepeated = daysRepeated;
     }
 }
