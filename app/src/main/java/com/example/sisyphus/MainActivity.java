650111255d61f3e8b79a56ca45c632f1f901f769
KaelenB <ktbrown@ualberta.ca>
2021-11-02 12:30:56 -0600

    small changes to test and CalendarActivity

--- app/src/main/java/com/example/sisyphus/MainActivity.java
+++ app/src/main/java/com/example/sisyphus/MainActivity.java
@@ -2,19 +2,25 @@ package com.example.sisyphus;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
 import android.content.Intent;
+import android.os.Bundle;
+
 import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
 
+    private FirebaseAuth mauth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        FirebaseAuth mauth = new FirebaseAuth();
+
+        mauth = FirebaseAuth.getInstance();
+        mauth.signInWithEmailAndPassword("ktbrown@ualberta.ca", "123456");
         Intent i = new Intent(MainActivity.this, CalendarActivity.class);
-        i.putExtra("MAUTH", mauth);
+        startActivity(i);
+
+
     }
 }
