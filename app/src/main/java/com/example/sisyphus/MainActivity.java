5d1274e9abe13415f1183d59c79c00c611d67cf9
KaelenB <ktbrown@ualberta.ca>
2021-11-02 14:56:03 -0600

    tested

--- app/src/main/java/com/example/sisyphus/MainActivity.java
+++ app/src/main/java/com/example/sisyphus/MainActivity.java
@@ -1,24 +1,42 @@
 package com.example.sisyphus;
 
+import static java.util.List.of;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.Timestamp;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.time.DayOfWeek;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.EnumSet;
+import java.util.GregorianCalendar;
+import java.util.List;
+import java.util.Locale;
+import java.util.TimeZone;
 
 public class MainActivity extends AppCompatActivity {
-
+    private final String TAG = "Sample";
     private FirebaseAuth mauth;
+    @RequiresApi(api = Build.VERSION_CODES.O)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
-        mauth = FirebaseAuth.getInstance();
-        mauth.signInWithEmailAndPassword("ktbrown@ualberta.ca", "123456");
         Intent i = new Intent(MainActivity.this, CalendarActivity.class);
         startActivity(i);
 
