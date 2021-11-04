612c74bac1194eeb6ce50ac84c3e3ef1412de48a
Gerrinator <vangenderengerard@gmail.com>
2021-11-03 18:40:36 -0600

    Trying to fix whats causing tasks to fail

---
+++ app/src/test/java/com/example/sisyphus/HabitClassTest.java
@@ -0,0 +1,36 @@
+package com.example.sisyphus;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+import com.example.sisyphus.Model.Habit;
+
+import java.util.ArrayList;
+import java.util.Date;
+
+/**
+ * A test class to test the 'Habit' class.
+ */
+public class HabitClassTest {
+
+    private Habit mockHabit(){
+        Habit habit = new Habit();
+        habit.setHabitName("Sleeping");
+        habit.setStartDate(new Date());
+        ArrayList<String> frequency = new ArrayList<>();
+        frequency.add("TUESDAY");
+        frequency.add("WEDNESDAY");
+        habit.setFrequency(frequency);
+        habit.setReason("Tired.");
+        return habit;
+    }
+
+    @Test
+    public void testGetHabitName(){
+        assertEquals(mockHabit().getHabitName(), "Sleeping");
+    }
+
+
+}
