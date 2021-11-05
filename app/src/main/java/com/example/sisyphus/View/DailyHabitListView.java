77500309559e91e7e1febccade925f433c47444b
sihaaan <ibrahim4@ualberta.ca>
2021-11-04 23:00:06 -0600

    -Created the UI for new Activity DailyHabitListView -Updated the colors.xml to have consistent colors throughout

---
+++ app/src/main/java/com/example/sisyphus/View/DailyHabitListView.java
@@ -0,0 +1,16 @@
+package com.example.sisyphus.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.sisyphus.R;
+
+public class DailyHabitListView extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_daily_habit_list_view);
+    }
+}
